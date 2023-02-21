2b57b0e4e0511220d331e7f40300eb193327c8b7
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-02-21 13:53:22 -0300

    adicionei os useStates de cada input

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import FormularioCliente from './components/FormularioCliente';
+import {useState} from 'react';
+import CadastroContato from "./components/CadastroContato";
+import { IContato } from './types/IContato';
 
 function App() {
-  return <FormularioCliente />;
+  const [nome, setNome] = useState<string>('');
+  const [emails, setEmails] = useState<string[]>(['']);
+  const [numeros, setNumeros] = useState<string[]>(['']);
+  const [cnpj, setCnpj] = useState<string>('');
+  const [cep, setCep] = useState<string>('');
+
+  const [contatos, setContatos] = useState<IContato[]>([]);
+
+  return (
+    <>
+      <CadastroContato
+        nome={{valor: nome, setValor: setNome, tipo: 'normal'}}
+        emails={{valores: emails, setValores: setEmails, tipo: 'array'}}
+        numeros={{valores: numeros, setValores: setNumeros, tipo: 'array'}}
+        cnpj={{valor: cnpj, setValor: setCnpj, tipo: 'normal'}}
+        cep={{valor: cep, setValor: setCep, tipo: 'normal'}}
+        contatos={contatos}
+        setContatos={setContatos}
+      />
+    </>
+  )
 }
 
 export default App;
