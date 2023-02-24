2dcfea95bc2ec1e7c52f707e3fb4d6ee16bec520
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-02-24 12:50:54 -0300

    tirei a obrigatoriedade de alguns campos

--- src/types/IContato.ts
+++ src/types/IContato.ts
@@ -1,9 +1,9 @@
 export interface IContato {
   nome: string,
-  nomeEmpresa: string,
-  emails: string[],
-  numeros: string[],
-  cnpj: string,
-  cep: string,
+  nomeEmpresa?: string,
+  emails?: string[],
+  numeros?: string[],
+  cnpj?: string,
+  cep?: string,
   id: number
 }
