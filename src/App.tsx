a4a913aa3890110239f24529ab98e77c5603242f
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-02-23 22:54:06 -0300

    agora os contatos são armazenados no local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import {useState} from 'react';
-import CadastroContato from "./components/CadastroContato";
+import { useEffect, useState } from 'react';
+import CadastroContato from './components/CadastroContato';
 import Contatos from './components/Contatos';
 import { IContato } from './types/IContato';
 
@@ -10,24 +10,33 @@ function App() {
   const [cnpj, setCnpj] = useState<string>('');
   const [cep, setCep] = useState<string>('');
 
-  const [contatos, setContatos] = useState<IContato[]>([]);
+  const [contatos, setContatos] = useState<IContato[]>(contatosLocalStorage());
+
+  function contatosLocalStorage(): IContato[] {
+    const item = localStorage.getItem('contatos');
+    if (item) {
+      return JSON.parse(item);
+    } else return [];
+  }
+
+  useEffect(() => {
+    localStorage.setItem('contatos', JSON.stringify(contatos));
+  }, [contatos]);
 
   return (
     <>
       <CadastroContato
-        nome={{valor: nome, setValor: setNome, tipo: 'normal'}}
-        emails={{valores: emails, setValores: setEmails, tipo: 'array'}}
-        numeros={{valores: numeros, setValores: setNumeros, tipo: 'array'}}
-        cnpj={{valor: cnpj, setValor: setCnpj, tipo: 'normal'}}
-        cep={{valor: cep, setValor: setCep, tipo: 'normal'}}
+        nome={{ valor: nome, setValor: setNome, tipo: 'normal' }}
+        emails={{ valores: emails, setValores: setEmails, tipo: 'array' }}
+        numeros={{ valores: numeros, setValores: setNumeros, tipo: 'array' }}
+        cnpj={{ valor: cnpj, setValor: setCnpj, tipo: 'normal' }}
+        cep={{ valor: cep, setValor: setCep, tipo: 'normal' }}
         contatos={contatos}
         setContatos={setContatos}
       />
-      <Contatos
-        contatos={contatos}
-      />
+      <Contatos contatos={contatos} setContatos={setContatos} />
     </>
-  )
+  );
 }
 
 export default App;
