86b20f842f03615ca2cac7d3d858332c1f3a3f22
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-02-24 12:45:49 -0300

    agora, caso os campos estiverem vazios, o contato não irá ter o campo, apenas o nome é obrigatório

--- src/components/CadastroContato/index.tsx
+++ src/components/CadastroContato/index.tsx
@@ -5,6 +5,7 @@ import { Inputs } from '../../types/Inputs';
 import InputsFormulario from './InputsFormulario';
 import { IContato } from '../../types/IContato';
 import OutrasOpcoes from './OutrasOpcoes';
+import { useState } from 'react';
 
 interface Props {
   nome: IState;
@@ -28,7 +29,17 @@ export default function CadastroContato({
   setContatos,
 }: Props) {
   const inputs: Inputs = [nome, nomeEmpresa, emails, numeros, cnpj, cep];
+  const [erro, setErro] = useState(false);
+  if (erro === true) {
+    if (nome.valor !== '') {
+      setErro(false);
+    }
+  }
   function handleSubmit() {
+    if (nome.valor === '') {
+      setErro(true);
+      return;
+    }
     if (
       nome.valor !== undefined &&
       nomeEmpresa.valor !== undefined &&
@@ -46,16 +57,21 @@ export default function CadastroContato({
           numero++;
         }
       }
-      const contato: IContato = {
+      let contato: IContato = {
         nome: nome.valor,
-        nomeEmpresa: nomeEmpresa.valor,
-        emails: emails.valores,
-        numeros: numeros.valores,
-        cnpj: cnpj.valor,
-        cep: cep.valor,
         id: id,
       };
+      nomeEmpresa.valor !== '' && (contato.nomeEmpresa = nomeEmpresa.valor);
+      emails.valores[0] !== '' && (contato.emails = emails.valores);
+      numeros.valores[0] !== '' && (contato.numeros = numeros.valores);
+      cnpj.valor !== '' && (contato.cnpj = cnpj.valor);
+      cep.valor !== '' && (contato.cep = cep.valor);
+
       setContatos([...contatos, contato]);
+      inputs.forEach((item) => {
+        item.setValor ? item.setValor('') : '';
+        item.setValores ? item.setValores(['']) : '';
+      })
     } else {
       throw Error('Erro no uso da função handleSubmit.');
     }
@@ -63,7 +79,7 @@ export default function CadastroContato({
   return (
     <div className={styles.formularioDiv}>
       <Paper elevation={3} className={styles.formulario}>
-        <InputsFormulario inputs={inputs} />
+        <InputsFormulario inputs={inputs} erro={erro} />
         <Button
           variant='contained'
           sx={{ width: '100%', minHeight: '3.5rem' }}
