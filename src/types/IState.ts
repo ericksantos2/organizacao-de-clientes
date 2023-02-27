0d115e5a61b4709be7f08ae4115fa8a1170c7937
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-02-27 11:30:18 -0300

    adicionei algumas outras coisas na interface

--- src/types/IState.ts
+++ src/types/IState.ts
@@ -4,4 +4,9 @@ export interface IState {
   setValor?: React.Dispatch<React.SetStateAction<string>>,
   valores?: string[],
   setValores?: React.Dispatch<React.SetStateAction<string[]>>,
+  required?: boolean,
+  nome?: string,
+  texto?: string,
+  mascara?: (value: string) => string,
+  temErro?: boolean,
 }
