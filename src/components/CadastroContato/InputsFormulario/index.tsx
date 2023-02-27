76a29112f7dde1b09c67a2b48cabf97b70d53575
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-02-27 11:27:15 -0300

    refatorei o código para ficar mais facil de entender e mais organizado

--- src/components/CadastroContato/InputsFormulario/index.tsx
+++ src/components/CadastroContato/InputsFormulario/index.tsx
@@ -1,5 +1,4 @@
 import { TextField } from '@mui/material';
-import { Inputs } from '../../../types/Inputs';
 import { IState } from '../../../types/IState';
 import Botao, { handleBotaoAdicionar } from './BotaoAdicionar';
 import styles from './InputsFormulario.module.scss';
@@ -11,93 +10,61 @@ function handleValue(
 ) {
   if (item.valores && item.setValores) {
     let itemAntes = [...item.valores];
-    itemAntes[index] = evento.target.value;
+    let input = evento.target.value;
+    if (item.mascara) {
+      input = item.mascara(input);
+    }
+    itemAntes[index] = input;
     item.setValores([...itemAntes]);
   } else {
     throw Error('Erro no uso da função');
   }
 }
 
-let valorAntes = '';
-
 export default function InputsFormulario({
   inputs,
   erro = false,
 }: {
-  inputs: Inputs;
+  inputs: IState[];
   erro: boolean;
 }) {
-  const nomes: string[] = [
-    'Nome',
-    'Nome da Empresa',
-    'Email',
-    'Numero',
-    'CNPJ',
-    'CEP',
-  ];
-  function cnpjMask(value: string) {
-    // Eu sei que talvez dê pra usar expressões regulares, mas assim fica mais simples
-    if (!value) return ''
-    value = value.replace(/[a-zA-Zç]/g, '')
-    if ((valorAntes.length === 1 && value.length === 2) || (valorAntes.length === 5 && value.length === 6)) {value += '.'};
-    if (valorAntes.length === 9 && value.length === 10) {value += '/'};
-    if (valorAntes.length === 14 && value.length === 15) {value += '-'};
-    if (value.length >= 18) {return value.substring(0,18)};
-    valorAntes = value;
-    return value;
-  }
   return (
     <>
       {inputs.map((item, index) => {
-        if (index === 0) {
+        if (item.tipo === 'normal') {
           return (
             <TextField
               key={index}
-              label={nomes[index]}
+              label={item.texto}
               value={item.valor ? item.valor : ''}
-              onChange={(evento) => {
-                item.setValor ? item.setValor(evento.target.value) : '';
-              }}
-              className={styles.input}
-              error={erro === true ? true : false}
-              helperText={erro === true ? 'Campo obrigatório' : ''}
-              required
-            />
-          );
-        } else if (nomes[index] === 'CNPJ') {
-          return (
-            <TextField
-              className={styles.input}
-              label={nomes[index]}
-              value={item.valor}
               onChange={(evento) => {
                 if (item.setValor) {
                   let input = evento.target.value;
-                  input = cnpjMask(input);
+                  if (item.mascara) {
+                    input = item.mascara(input);
+                  }
                   item.setValor(input);
                 }
               }}
-            />
-          )
-        } else if (item.tipo === 'normal') {
-          return (
-            <TextField
-              key={index}
-              label={nomes[index]}
-              value={item.valor ? item.valor : ''}
-              onChange={(evento) => {
-                item.setValor ? item.setValor(evento.target.value) : '';
-              }}
               className={styles.input}
+              error={item.temErro && (erro === true ? true : false)}
+              helperText={
+                item.temErro
+                  ? erro === true
+                    ? 'Campo obrigatório'
+                    : false
+                  : false
+              }
+              required={item.required === true}
             />
           );
-        } else if (item.valores) {
-          return item.valores.map((itemB, indexB) => {
+        } else if (item.tipo === 'array') {
+          return item.valores?.map((itemB: string, indexB: number) => {
             if (indexB === 0) {
               return (
                 <div className={styles.input__array} key={indexB}>
                   <TextField
-                    label={nomes[index]}
+                    label={item.texto}
                     value={itemB}
                     onChange={(evento) => {
                       handleValue(evento, item, indexB);
@@ -105,13 +72,11 @@ export default function InputsFormulario({
                     sx={{ width: '100%' }}
                   />
                   <Botao
-                    disabled={
-                      item.valores
-                        ? item.valores.length >= 5
-                          ? 'true'
-                          : 'false'
-                        : 'false'
-                    }
+                    desabilitado={{
+                      valor: item.valores ? item.valores : [''],
+                      tipo: 'maior',
+                      numero: 5,
+                    }}
                     onClick={() => {
                       handleBotaoAdicionar(item, 'adicionar');
                     }}
@@ -119,13 +84,11 @@ export default function InputsFormulario({
                     +
                   </Botao>
                   <Botao
-                    disabled={
-                      item.valores
-                        ? item.valores.length === 1
-                          ? 'true'
-                          : 'false'
-                        : 'false'
-                    }
+                    desabilitado={{
+                      valor: item.valores ? item.valores : [''],
+                      tipo: 'igual',
+                      numero: 1,
+                    }}
                     onClick={() => {
                       handleBotaoAdicionar(item, 'remover');
                     }}
@@ -134,19 +97,18 @@ export default function InputsFormulario({
                   </Botao>
                 </div>
               );
-            } else {
-              return (
-                <TextField
-                  key={indexB}
-                  label={nomes[index]}
-                  value={itemB}
-                  onChange={(evento) => {
-                    handleValue(evento, item, indexB);
-                  }}
-                  sx={{ width: '100%' }}
-                />
-              );
             }
+            return (
+              <TextField
+                key={indexB}
+                label={item.texto}
+                value={itemB}
+                onChange={(evento) => {
+                  handleValue(evento, item, indexB);
+                }}
+                sx={{ width: '100%' }}
+              />
+            );
           });
         }
       })}
